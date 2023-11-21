d99931a7f8ed40707448ac09493e4b823e85395a
sandeep <38496388+amigopsycho@users.noreply.github.com>
2023-11-21 06:50:23 +0000

    first

---
+++ Auth-UI/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './Features/login/login.component';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+  imports: [CommonModule, RouterOutlet, LoginComponent]
+})
+export class AppComponent {
+  title = 'Auth-UI';
+}
